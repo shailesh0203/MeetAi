89e67e65b4d464603ef6b171f99bb3016ade3ac4
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-09-14 04:30:24 +0530

    09 Agents setup

--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -1,18 +1,14 @@
 "use client"
 
-import { useTRPC } from "@/trpc/client"
-import { useQueries, useQuery } from "@tanstack/react-query"
-import { use } from "react"
 
 
 export const HomeView = () => {
   
-  const trpc=useTRPC()
-  const {data}=useQuery(trpc.hello.queryOptions({text:"Shailesh"}))
+
 
   return (
-    <div className="flex flex-col p-4 gap-y-4">
-{data?.greeting}
+    <div>
+      Home View
     </div>
   )
 }
