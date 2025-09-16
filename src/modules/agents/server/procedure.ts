27b52a6f2c723a420a6caa16884e405113344da3
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-09-16 06:28:21 +0530

    14 agent page

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -10,9 +10,14 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 
 export const agentsRouter=createTRPCRouter({
     //todo:change
-     getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input})=>{
-        const [existingAgent]=await db.select({meetingCount:sql<number>`5`,...getTableColumns(agents),}).from(agents).where(eq(agents.id, input.id));
-
+     getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input,ctx})=>{
+        const [existingAgent]=await db.select({meetingCount:sql<number>`5`,...getTableColumns(agents),}).from(agents).where(and(
+            eq(agents.id, input.id),
+            eq(agents.userId,ctx.auth.user.id)
+        ));
+        if(!existingAgent){
+            throw new TRPCError({code:"NOT_FOUND",message:"Ageny not found"})
+        }
         return existingAgent;
     }),
     getMany:protectedProcedure
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -12,10 +12,12 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agent-filter";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 
 
 export const AgentsView=()=>{
+    const router=useRouter();
 const [filters,setFilters]=useAgentsFilters();
 
     const trpc=useTRPC();
@@ -27,7 +29,11 @@ const [filters,setFilters]=useAgentsFilters();
     
     return(
        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-       <DataTable data={data.items} columns={columns}/>
+       <DataTable 
+       data={data.items} 
+       columns={columns}
+       onRowClick={(row)=>router.push(`/agents/${row.id}`)}
+       />
        <DataPagination
        page={filters.page}
        totalPages={data.totalPages}
