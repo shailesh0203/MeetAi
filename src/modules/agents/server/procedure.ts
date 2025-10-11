122ce39d0e84c7c57200a48a83c61e02c3f64168
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-10-11 20:14:03 +0530

    15-agent-update-delete

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { agentsInsertSchema } from "../schema";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schema";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
@@ -10,6 +10,48 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 
 export const agentsRouter=createTRPCRouter({
     //todo:change
+    update: protectedProcedure
+  .input(agentsUpdateSchema)
+  .mutation(async ({ ctx, input }) => {
+    const [updatedAgent] = await db
+      .update(agents)
+      .set(input)
+      .where(
+        and(
+          eq(agents.id, input.id),
+          eq(agents.userId, ctx.auth.user.id),
+        ),
+      )
+      .returning()
+      if(!updatedAgent){
+         throw new TRPCError({
+          code:"NOT_FOUND",
+          message:"Agent not found"
+        })
+      }
+       return updatedAgent
+  }),
+
+    remove:protectedProcedure
+    .input(z.object({id:z.string()}))
+    .mutation(async({ctx,input})=>{
+      const [removedAgent]=await db
+      .delete(agents)
+      .where(
+        and(
+          eq(agents.id,input.id),
+          eq(agents.userId,ctx.auth.user.id),
+        ),
+      )
+      .returning();
+      if(!removedAgent){
+        throw new TRPCError({
+          code:"NOT_FOUND",
+          message:"Agent not found"
+        })
+      }
+      return removedAgent
+    }),
      getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input,ctx})=>{
         const [existingAgent]=await db.select({meetingCount:sql<number>`5`,...getTableColumns(agents),}).from(agents).where(and(
             eq(agents.id, input.id),
--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -43,6 +43,24 @@ const createAgent = useMutation(
             trpc.agents.getMany.queryOptions({}),
         )
 
+       //TODO:Invalidate free tier usage
+        onSuccess?.()
+
+    },
+    onError: (error) => {
+        toast.error(error.message)
+
+        //TODO:check if error code is "Forbidden",redirect to "/upgrade"
+    },
+  }),
+);
+const updateAgent = useMutation(
+  trpc.agents.update.mutationOptions({
+    onSuccess:async() => {
+        await queryClient.invalidateQueries(
+            trpc.agents.getMany.queryOptions({}),
+        )
+
         if(initialValues?.id){
             await queryClient.invalidateQueries(
                 trpc.agents.getOne.queryOptions({id:initialValues.id})
@@ -67,11 +85,11 @@ const form = useForm<z.infer<typeof agentsInsertSchema>>({
 });
 
 const isEdit = !!initialValues?.id;
-const isPending = createAgent.isPending;
+const isPending = createAgent.isPending|| updateAgent.isPending;
 
 const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
   if (isEdit) {
-    console.log("TODO: updateAgent");
+    updateAgent.mutate({...values,id:initialValues.id})
   } else {
     createAgent.mutate(values);
   }
