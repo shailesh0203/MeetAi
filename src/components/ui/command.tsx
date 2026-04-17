f4a4698a5284757a0039b7d46a2feaafc2d0568a
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2026-04-17 12:47:26 +0530

    17:meeting form

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -72,6 +72,7 @@ function CommandResponsiveDialog({
   title = "Command Palette",
   description = "Search for a command to run...",
   children,
+  shouldFilter=true,
   className,
   showCloseButton = true,
   ...props
@@ -80,6 +81,7 @@ function CommandResponsiveDialog({
   description?: string
   className?: string
   showCloseButton?: boolean
+  shouldFilter?:boolean
 }) {
   const isMobile=useIsMobile()
 
@@ -91,7 +93,7 @@ function CommandResponsiveDialog({
         <DrawerTitle>{title}</DrawerTitle>
         <DrawerDescription>{description}</DrawerDescription>
       </DrawerHeader>
-      <Command className="[&_cmdk-group-heading]:text-muted-foreground [&_cmdk-group-heading]:px-2 [&_cmdk-group]:px-2 [&_cmdk-group]:not([hidden])~[cmdk-group]:pt-0 [&_cmdk-input-wrapper_svg]:h-5 [&_cmdk-input-wrapper_svg]:w-5 [&_cmdk-input-wrapper]:h-12 [&_cmdk-item]:px-2 [&_cmdk-item]:py-3 [&_cmdk-item_svg]:h-5 [&_cmdk-item_svg]:w-5">
+      <Command shouldFilter={shouldFilter} className="[&_cmdk-group-heading]:text-muted-foreground [&_cmdk-group-heading]:px-2 [&_cmdk-group]:px-2 [&_cmdk-group]:not([hidden])~[cmdk-group]:pt-0 [&_cmdk-input-wrapper_svg]:h-5 [&_cmdk-input-wrapper_svg]:w-5 [&_cmdk-input-wrapper]:h-12 [&_cmdk-item]:px-2 [&_cmdk-item]:py-3 [&_cmdk-item_svg]:h-5 [&_cmdk-item_svg]:w-5">
         {children}
       </Command>
     </DrawerContent>
@@ -109,7 +111,7 @@ function CommandResponsiveDialog({
         className={cn("overflow-hidden p-0", className)}
         showCloseButton={showCloseButton}
       >
-        <Command className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command shouldFilter={shouldFilter} className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
       </DialogContent>
